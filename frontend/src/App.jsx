6892f7e16ffa58d282368cc37b0f95cdabb3e894
Miguel Augusto <miguelmasche@gmail.com>
2024-05-15 19:16:01 -0500

    created 3 photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,24 +3,47 @@
 import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+// const sampleDataForPhotoListItem = {
+//   id: "1",
+//   location: {
+//     city: "Montreal",
+//     country: "Canada",
+//   },
+//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+//   username: "Joe Example",
+//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+// };
 
 const App = () => {
+  // define the sample data for each PhotoListItem
+  const sampleDataForPhotoListItem = {
+    id: "1",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    username: "Joe Example",
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+  };
+
+  // create an array of photo data
+  const photos = new Array(3).fill(sampleDataForPhotoListItem).map((photo, index) => ({
+    ...photo,
+    id: index + 1,
+  }));
+
   return (
-    <div className="App">
-      <PhotoListItem photo={sampleDataForPhotoListItem} />
+    <div className="photo-list">
+      {photos.map(photo => (
+        <PhotoListItem key={photo.id} photo={photo} />
+      ))}
     </div>
   );
 };
+
+
+
 
 export default App;
 
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,38 +3,32 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-
-
-
 const PhotoListItem = ({ photo }) => {
   const { username, imageSource, id, location, profile } = photo;
   
   return (
     <div className="photo-list__item">
       <img 
-        className="photo-list__user-profile"
-        src={profile} 
-        alt={`${username}'s profile`} 
+        className="photo-list__image"
+        src={imageSource} 
+        alt={`Photo ${id}`} 
       />
       <div className="photo-list__user-details">
+        <img 
+          className="photo-list__user-profile"
+          src={profile} 
+          alt={`${username}'s profile`} 
+        />
         <div className="photo-list__user-info">
           <p>{username}</p>
           <p className="photo-list__user-location">{`${location.city}, ${location.country}`}</p>
         </div>
       </div>
-      <img 
-        className="photo-list__image"
-        src={imageSource} 
-        alt={`Photo ${id}`} 
-      />
     </div>
   );
 };
 
 
-
-
-// Export the component so it can be used in other files
 export default PhotoListItem;
 
 
