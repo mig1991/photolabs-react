9e27f419c8a5163c98edb73c2261fe73d91c371d
Miguel Augusto <miguelmasche@gmail.com>
2024-05-17 06:37:23 -0700

    add top navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TopNavigationBar from './components/TopNavigationBar';
 import PhotoList from './components/PhotoList';
-import TopicList from './components/TopicList';
 import './App.scss';
 
-const App = () => (
-  <div className="App">
-    <TopicList />
-    <PhotoList />
-  </div>
-);
+const App = () => {
+  // Example state to track if there are favorite photos
+  const [isFavPhotoExist, setIsFavPhotoExist] = useState(false);
+
+  return (
+    <div className="App">
+      <TopNavigationBar isFavPhotoExist={isFavPhotoExist} />
+      <PhotoList />
+    </div>
+  );
+};
 
 export default App;
 
