6339735371ee8ab529aebeb498e87bc8c99bedfd
Miguel Augusto <miguelmasche@gmail.com>
2024-05-18 18:06:01 -0700

    fix bug

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -1,14 +1,20 @@
+// src/components/FavBadge.jsx
+
 import React from 'react';
 import FavIcon from './FavIcon';
-
 import '../styles/FavBadge.scss';
 
-const FavBadge = ({ isFavPhotoExist }) => {
+const FavBadge = ({ isFavPhotoExist, favouritesCount }) => {
   return (
     <div className='fav-badge'>
-      <FavIcon displayAlert={!!isFavPhotoExist}/>
+      {isFavPhotoExist && (
+        <>
+          <FavIcon isLiked={true} />
+          <span className="fav-badge__count">{favouritesCount}</span>
+        </>
+      )}
     </div>
-  ) 
+  ); 
 };
 
 export default FavBadge;
