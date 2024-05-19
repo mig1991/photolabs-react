dc41b9a48fec37084d555dd4136e41c5ab801589
Miguel Augusto <miguelmasche@gmail.com>
2024-05-19 12:33:01 -0700

    add user details and similar photos header under enlarged photo in modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -14,7 +14,22 @@ const PhotoDetailsModal = ({ onClose, photo }) => {
       </button>
       <div className="photo-details-modal__content">
         <img src={photo.urls.regular} alt={photo.title} className="photo-details-modal__image" />
-        {/* Placeholder for additional content */}
+        <div className="photo-details-modal__header">
+          {photo.title}
+        </div>
+        <div className="photo-details-modal__user-details photo-list__user-details">
+          <img
+            className="photo-list__user-profile"
+            src={photo.user.profile}
+            alt={`${photo.user.username}'s profile`}
+          />
+          <div className="photo-list__user-info">
+            <p>{photo.user.username}</p>
+            <p className="photo-list__user-location">{`${photo.location.city}, ${photo.location.country}`}</p>
+          </div>
+        </div>
+        <h2 className="photo-details-modal__similar-header">Similar Photos</h2>
+        {/* Placeholder for similar photos */}
         <div className="photo-details-modal__additional-content">
           {/* Additional content goes here */}
         </div>
