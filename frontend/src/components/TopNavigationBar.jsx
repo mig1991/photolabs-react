6f84c85791f0d8e4c3e90aa80120be28ecd18166
Miguel Augusto <miguelmasche@gmail.com>
2024-05-21 11:16:55 -0700

    add search bar and scss styles

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -2,20 +2,44 @@ import React from 'react';
 import NavFavIcon from './NavFavIcon';
 import TopicList from './TopicList';
 import '../styles/TopNavigationBar.scss';
+import SearchBar from './ SearchBar';
 
 const TopNavigationBar = ({ topics, fetchPhotosByTopic }) => {
-  return (
-    <div className="top-nav-bar">
-      <span className="top-nav-bar__logo">PhotoLabs</span>
-      <div className="nav-topics">
-        <TopicList topics={topics} fetchPhotosByTopic={fetchPhotosByTopic} />
-      </div>
-      <div className="favourites-notification">
-        <NavFavIcon />
-      </div>
-    </div>
-  );
-};
 
+  const handleSearch = (query) => {
+    // Implement the search logic here
+    console.log(`Searching for ${query}`);
+  };
+
+
+//   return (
+//     <div className="top-nav-bar">
+//       <span className="top-nav-bar__logo">PhotoLabs</span>
+//       <div className="nav-topics">
+//         <TopicList topics={topics} fetchPhotosByTopic={fetchPhotosByTopic} />
+//       </div>
+//       <div className="nav-search">
+//         <SearchBar onSearch={handleSearch} />
+//       </div>
+//       <div className="favourites-notification">
+//         <NavFavIcon />
+//       </div>
+//     </div>
+//   );
+// };
+
+return (
+  <div className="top-nav-bar">
+    <span className="top-nav-bar__logo">PhotoLabs</span>
+    <div className="nav-center">
+      <TopicList topics={topics} fetchPhotosByTopic={fetchPhotosByTopic} />
+    </div>
+    <div className="nav-right">
+      <SearchBar onSearch={handleSearch} />
+      <NavFavIcon />
+    </div>
+  </div>
+);
+};
 
 export default TopNavigationBar;
